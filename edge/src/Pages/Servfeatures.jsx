5d0391f150043623374dab73da96d480d5480b93
Monk19 <ravigadu.19@gmail.com>
2021-10-03 15:16:48 +0530

    serv feartues

---
+++ edge/src/Pages/Servfeatures.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function Servfeatures() {
+  return (
+    <div className="serv-features">
+      <div>
+        <img />
+        <h1>Staff Augmentation</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tLorem ipsum, or lipsum as it is sometimes known, is dummy
+          text used in laying out print, graphic or web designs. The passage is
+          attributed to an unknown typesetter in the 15th century who is thought
+          to have.
+        </p>
+        <button>read more</button>
+      </div>
+      <div>
+        <img />
+        <h1>Staff Augmentation</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tLorem ipsum, or lipsum as it is sometimes known, is dummy
+          text used in laying out print, graphic or web designs. The passage is
+          attributed to an unknown typesetter in the 15th century who is thought
+          to have.
+        </p>
+        <button>read more</button>
+      </div>
+      <div>
+        <img />
+        <h1>Staff Augmentation</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tLorem ipsum, or lipsum as it is sometimes known, is dummy
+          text used in laying out print, graphic or web designs. The passage is
+          attributed to an unknown typesetter in the 15th century who is thought
+          to have.
+        </p>
+        <button>read more</button>
+      </div>
+      <div>
+        <img />
+        <h1>Staff Augmentation</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tLorem ipsum, or lipsum as it is sometimes known, is dummy
+          text used in laying out print, graphic or web designs. The passage is
+          attributed to an unknown typesetter in the 15th century who is thought
+          to have.
+        </p>
+        <button>read more</button>
+      </div>
+    </div>
+  );
+}
